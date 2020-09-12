8c0625a93466efd80d9566ac37127fc5b4580531
AlekseiKorolev <42728999+AlekseiKorolev@users.noreply.github.com>
2020-09-13 01:35:59 +0200

    Updates

---
+++ precache-manifest.8db2113ec5ed9f0a9cacd08de74ee6c8.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc9388e22017e72b4fe9c29988e2e436",
+    "url": "/kipos/index.html"
+  },
+  {
+    "revision": "fb1186fe69f2b8b31119",
+    "url": "/kipos/static/css/main.53bb8713.chunk.css"
+  },
+  {
+    "revision": "5f4a6ecf324b438d58a2",
+    "url": "/kipos/static/js/2.4bf870ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kipos/static/js/2.4bf870ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb1186fe69f2b8b31119",
+    "url": "/kipos/static/js/main.783bf8b3.chunk.js"
+  },
+  {
+    "revision": "205465923e61bc32e649",
+    "url": "/kipos/static/js/runtime-main.829a11bf.js"
+  },
+  {
+    "revision": "165b3175b1345c0eb8b4097f4d024455",
+    "url": "/kipos/static/media/Century.165b3175.ttf"
+  },
+  {
+    "revision": "1ecf9da93d85d7f6c55d9f40a5623a3e",
+    "url": "/kipos/static/media/Century.1ecf9da9.eot"
+  },
+  {
+    "revision": "280a927a5f3172b7bb6bd79648f13df7",
+    "url": "/kipos/static/media/Century.280a927a.svg"
+  },
+  {
+    "revision": "a5befb3c4f5eee651eb793d64febf2af",
+    "url": "/kipos/static/media/Century.a5befb3c.woff"
+  },
+  {
+    "revision": "b063ccdf84f300c85928932f4f7019c2",
+    "url": "/kipos/static/media/Century.b063ccdf.woff2"
+  },
+  {
+    "revision": "03fb56501793dc7f72a44b15e84c716c",
+    "url": "/kipos/static/media/MuseoSansCyrl-500.03fb5650.ttf"
+  },
+  {
+    "revision": "4c464bbca18508723077a7e262662bc9",
+    "url": "/kipos/static/media/MuseoSansCyrl-500.4c464bbc.svg"
+  },
+  {
+    "revision": "577120d79b21c2b201917ea31f9a46b0",
+    "url": "/kipos/static/media/MuseoSansCyrl-500.577120d7.woff2"
+  },
+  {
+    "revision": "9c312b5e006c94d18d8f995a3c952a0f",
+    "url": "/kipos/static/media/MuseoSansCyrl-500.9c312b5e.woff"
+  },
+  {
+    "revision": "db0c88fedde0a5ae7c7774b4cdb1a987",
+    "url": "/kipos/static/media/MuseoSansCyrl-500.db0c88fe.eot"
+  },
+  {
+    "revision": "50690e08da3f0d73d43a4774997e2df3",
+    "url": "/kipos/static/media/calc.50690e08.png"
+  },
+  {
+    "revision": "4d37ffe7fb7daaded3e8d50cd413e451",
+    "url": "/kipos/static/media/infoArch.4d37ffe7.png"
+  },
+  {
+    "revision": "38420c9a431bbd0cb24ddc0307e389fb",
+    "url": "/kipos/static/media/infoDesign.38420c9a.png"
+  },
+  {
+    "revision": "a0868b2259b36d86031faf071d5ef36c",
+    "url": "/kipos/static/media/infoLand.a0868b22.png"
+  },
+  {
+    "revision": "7f4f8a1c0d71c5070c3cc8b3b2aac5d2",
+    "url": "/kipos/static/media/project.7f4f8a1c.png"
+  }
+]);
